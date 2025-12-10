252a3d6acc618bcbd42f2cbab3fba2622ea45806
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-10 06:56:48 +0300

    Update IPNS-republisher.sh

--- IPNS-republisher.sh
+++ IPNS-republisher.sh
@@ -3,5 +3,5 @@ cd "$(dirname "$0")"
 ./ipfs init
 ./ipfs daemon &
 sleep 10
-./ipfs routing get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
-curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
+./ipfs routing get /ipns/link > record.bin
+curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/link -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
