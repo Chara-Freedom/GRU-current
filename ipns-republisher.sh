896f84471f200a7f830d98b75f09c959dc426f95
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-12 15:58:02 +0300

    Update ipns-republisher.sh
    Conceptually using the republisher to maintain multiple files

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -7,5 +7,7 @@ while pgrep -x ld-linux-x86-64 > /dev/null
 do
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/link > record.bin
 curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/link -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/link2 > record.bin
+curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/link2 -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
 sleep 1d
 done
